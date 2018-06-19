e674b405c4b7f5d884b975653bd4fdaf8c0a70bc
徐俊全 <389203160@qq.com>
2018-06-19 10:38:37 +0800

    6.18添加设置页面----徐俊全

---
+++ ruyiruyiios/My/View/MySettingTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  MySettingTableViewCell.h
+//  ruyiruyiios
+//
+//  Created by xujunquan on 2018/6/19.
+//  Copyright © 2018年 ruyiruyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MySettingTableViewCell : UITableViewCell
+
+@property(nonatomic, strong)UIImageView *iconImageV;
+@property(nonatomic, strong)UILabel *titleLabel;
+@property(nonatomic, strong)UIImageView *rightImage;
+@property(nonatomic, strong)UIView *underLineView;
+@end
---
+++ ruyiruyiios/My/View/MySettingTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  MySettingTableViewCell.m
+//  ruyiruyiios
+//
+//  Created by xujunquan on 2018/6/19.
+//  Copyright © 2018年 ruyiruyi. All rights reserved.
+//
+
+#import "MySettingTableViewCell.h"
+
+@implementation MySettingTableViewCell
+
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
